39b67a138e9d916ef84c544297a96db49dcc7c4b
nicolasdemol <nyco.demol@gmail.com>
2023-10-22 01:43:44 +0200

    gpt-3.5

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -73,7 +73,7 @@ export const POST = async (req: NextRequest) => {
   }));
 
   const response = await openai.chat.completions.create({
-    model: "gpt-4",
+    model: "gpt-3.5-turbo",
     temperature: 0.5,
     stream: true,
     messages: [
